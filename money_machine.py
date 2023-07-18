4a3904ce97867793ddf816bcbda16dcc85e7c7ee
rafif <rafifabdussalam1@gmail.com>
2023-07-18 21:46:09 +0700

    edit money_machine

--- money_machine.py
+++ money_machine.py
@@ -24,4 +24,7 @@ class MoneyMachine:
             self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
         return self.money_received
 
+
+print("hello world")
+
     
